b1a661fd74812eeee7ff77785a3816ea56262fd9
jlam01 <jlam01@stuy.edu>
2019-03-11 13:36:26 -0400

    fixed duplicate

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -113,7 +113,7 @@ public class List_inArraySlots {
      // for (int i = index ; i <= filledElements; i++) {
      //     elements[endIndex + 1] = elements[endIndex];
      //     endIndex--;
-     // }
+     // } 
      // filledElements++;
      // elements[index] = value;
      //
@@ -129,22 +129,7 @@ public class List_inArraySlots {
          elements[index] = value;
          filledElements++;
      }
-=======
-     public void add (int index, int value) {
-         if (filledElements + 1 == elements.length) {
-             expand();
-         }
-         int endIndex = filledElements;
-         
-         for (int i = index ; i <= filledElements; i++) {
-             elements[endIndex + 1] = elements[endIndex];
-             endIndex--;
-         }
-         filledElements++;
-         elements[index] = value;
-             
-         }
->>>>>>> 192d5261573ae4b24ec8f873418422f85cce37d2
+
 
 
     /**
